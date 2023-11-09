e9a961d4aa9aa3de003196802cbc4e7509903dfa
Andrew Garrett <andrew@werdn.us>
2023-11-09 23:32:15 +1100

    Fix compiler errors.

--- pkg/framework/plugins/podlifetime/validation.go
+++ pkg/framework/plugins/podlifetime/validation.go
@@ -21,9 +21,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 // ValidatePodLifeTimeArgs validates PodLifeTime arguments
